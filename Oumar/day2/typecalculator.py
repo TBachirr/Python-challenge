89a748b017a3868c788eacdd950431cd3b56dcb9
creepypers <131252003+creepypers@users.noreply.github.com>
2024-07-09 00:48:59 -0400

    Add files via upload

---
+++ Oumar/day2/typecalculator.py
@@ -0,0 +1,3 @@
+number = input("Enter your number: ")
+number_modified = int(number[0])+int(number[-1])
+print(f"Your new number is {number_modified}")
---
+++ Oumar/day2/year.py
@@ -0,0 +1,6 @@
+
+age = int(input("Enter your age: "))
+year = 90 - age
+month = year * 12
+day = year * 365
+print(f"You have {day} days, {month} months and {year} years left")
