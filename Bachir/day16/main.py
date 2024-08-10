7f69c4e2b1961e5c9971fc95d77839e028c5797c
TBachirr <ibrahimabachirtraore@gmail.com>
2024-08-10 14:48:32 +0000

    Completed day 16

---
+++ Bachir/day16/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+tim = Turtle()
+print(tim)
+tim.shape("turtle")
+tim.color("cornflowerblue")
+tim.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
