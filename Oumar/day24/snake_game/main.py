53e932797e0f2709bef4801ea02c31d131067d2d
creepypers <131252003+creepypers@users.noreply.github.com>
2024-08-20 20:28:33 -0400

    Add files via upload

---
+++ Oumar/day24/snake_game/main.py
@@ -0,0 +1,42 @@
+import time
+import snake
+import food
+import scoreboard
+from turtle import Screen
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("snake game")
+screen.tracer(0)
+
+game_on = True
+snak=snake.Snake()
+foo =food.Food()
+score = scoreboard.Scoreboard()
+
+screen.listen()
+screen.onkey(snak.up,"Up")
+screen.onkey(snak.down,"Down")
+screen.onkey(snak.left,"Left")
+screen.onkey(snak.right,"Right")
+
+while game_on:
+    snak.move() 
+    screen.update()
+    time.sleep(0.1)
+    
+    if snak.head.distance(foo) < 15:
+        foo.refresh()  
+        snak.extend()
+        score.increase_score()
+        
+    if snak.head.xcor() > 280 or snak.head.xcor() < -280 or snak.head.ycor() > 280 or snak.head.ycor() < -280:
+        score.reset()
+        snak.reset()
+        
+    for seg in snak.segment[1:]:
+        if snak.head.distance(seg) < 10:
+            score.reset()
+            snak.reset() 
+        
+screen.exitonclick() 
