f5c67dd97a03ca4f5b4060767e18992d46a09628
Bachir Ibrahima Traore <ibrahimabachirtraore@gmail.com>
2024-08-23 15:56:35 +0000

    Completed challenge 24

--- Bachir/day20_21/snake.py
+++ Bachir/day20_21/snake.py
@@ -16,6 +16,13 @@ class Snake:
     def create_snake(self):
         for position in STARTING_POSITION:
             self.add_segment(position)
+        
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
             
     def add_segment(self,position):
